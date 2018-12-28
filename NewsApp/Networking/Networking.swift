370f746dfe7ee26c41ad446e9335c825ea755a22
Rio Rinaldi <riorinaldi@Rios-MacBook-Pro.local>
2018-12-28 17:06:35 +0700

    Commit

---
+++ NewsApp/Networking/Networking.swift
@@ -0,0 +1,9 @@
+//
+//  Networking.swift
+//  NewsApp
+//
+//  Created by Rio Rinaldi on 16/11/18.
+//  Copyright © 2018 Rio Rinaldi. All rights reserved.
+//
+
+import Foundation
---
+++ NewsApp/View/BeritaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeritaTableViewCell.swift
+//  NewsApp
+//
+//  Created by Rio Rinaldi on 16/11/18.
+//  Copyright © 2018 Rio Rinaldi. All rights reserved.
+//
+
+import UIKit
+
+class BeritaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
